a2702b09b5541ce3accfd8e343c89c3a0caa8ff5
cormacwit <20093840@mail.wit.ie>
2022-11-03 21:57:41 +0000

    Add to favourites logic completed; Dummy favourites page.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
-import sample from './stpries/sampleData'
-
-const movies = [sample, sample, sample, sample, sample, sample, sample];
+import MoviePage from "./pages/movieDetailsPage";
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import {Link} from 'react-router-dom'
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies/favourites">Favourites</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
